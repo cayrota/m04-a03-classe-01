cf6b630f02c533c46f2347cac0fd0e34d6e20613
cayrota <you@example.com>
2021-07-02 20:59:12 -0300

    conectando com o banco de dados heroku

--- src/conexao.js
+++ src/conexao.js
@@ -1,10 +1,11 @@
 const knex = require('knex')({
     client: 'pg',
     connection: {
-      host : 'localhost',
-      user : 'postgres',
-      password : '123456',
-      database : 'market_cubos'
+      host : 'ec2-54-163-97-228.compute-1.amazonaws.com',
+      user : 'mjqiovcpinrsmm',
+      password : '5de9cc8f6c729d647ebd5f7220db1c109e9a1ae8879eb499ea65798990605df0',
+      database : 'dc7477ecgbpf2b',
+      port: 5432
     }
   });
 
