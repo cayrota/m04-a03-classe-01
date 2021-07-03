57ae7a13599fb469ba80a985e98cf2d21e382d40
cayrota <you@example.com>
2021-07-02 21:49:55 -0300

    config ssl

--- src/conexao.js
+++ src/conexao.js
@@ -4,7 +4,10 @@ const knex = require('knex')({
       host : 'ec2-54-163-97-228.compute-1.amazonaws.com',
       user : 'mjqiovcpinrsmm',
       password : '5de9cc8f6c729d647ebd5f7220db1c109e9a1ae8879eb499ea65798990605df0',
-      database : 'dc7477ecgbpf2b'
+      database : 'dc7477ecgbpf2b',
+      ssl: {
+        rejectUnauthorized: false
+      }
     }
   });
 
